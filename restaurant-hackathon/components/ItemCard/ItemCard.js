5bb7a953dc62fc237541a8d215fda2cf8235bff3
Amaan Memon <amaan_memon@hotmail.co.uk>
2022-09-09 12:06:09 +0100

    card component started

--- restaurant-hackathon/components/ItemCard/ItemCard.js
+++ restaurant-hackathon/components/ItemCard/ItemCard.js
@@ -1,14 +1,45 @@
 import Styled from 'styled-components';
+import AddIcon from '@mui/icons-material/Add';
 
 const Item = Styled.div`
+width: 10rem;
+  height: 15rem;
+  position: relative;
+  &:hover {
+    cursor: grab;
+  }
+
+  > * {
+    border-radius: 3px;
+  }
+`;
+
+const ItemImage = Styled.img`
+  width: 10rem;
+  height: 10rem;
+
+`;
+
+const ItemName = Styled.h1`
+`;
+
+const ItemPrice = Styled.h2`
+`;
+
+const AddItemContainer = Styled.div`
 `;
 
 
-
-export const ItemCard = ({ item }) => {
+export const ItemCard = (data) => {
   return (
     <Item>
-      <div>{item}</div>
+      <ItemImage src="/Korean-Fried-Chicken.webp"/>
+      <ItemName>Korean Fried Chicken</ItemName>
+      <ItemPrice>£7</ItemPrice>
+      <AddItemContainer>
+        <AddIcon />
+      </AddItemContainer>
+      
       
     </Item>
   );
