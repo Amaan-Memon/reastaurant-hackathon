0964eb7a086f8787621c0f6e9d79b5e0a2ad06de
Joe Klakus <joeyk2k@hotmail.com>
2022-09-09 13:18:29 +0100

    header done

--- restaurant-hackathon/components/Header/Header.js
+++ restaurant-hackathon/components/Header/Header.js
@@ -1,13 +1,31 @@
-import Styled from "styled-components";
+import Styled from 'styled-components'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Image from 'next/image'
 
 const HeaderContainer = Styled.div`
+background: #203C75;
+height: 6.5rem;
+`;
 
+const ItemHolder = Styled.div`
+padding: 1rem;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
 `;
 
 export const Header = () => {
   return (
     <HeaderContainer>
-      <h1>header</h1>
+      <ItemHolder>
+      <Image src="/southKoreaFlag.png" height={60} width={120}></Image>
+      <h1>K-Chick</h1>
+      <ItemHolder >
+      <ShoppingCartIcon style={{ fontSize: 50 }}/>
+      <p style={{ fontSize: 10 }}>(1)</p>
+      </ItemHolder>
+      </ItemHolder>
     </HeaderContainer>
   );
 };
